7fc51d7c1c56ee3984c78f5818c3662626d40595
santanu <s.m.23102004@gmail.com>
2025-10-11 19:45:19 +0530

    completed the about section and add the partially done project section

--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,5 @@
 
 export const SITE_TITLE = "Coding Santa";
 export const SITE_DESCRIPTION = "Welcome to my website!";
+export const ABOUT_ME =
+  "I’m a software developer passionate on building reliable, real-time applications. I enjoy solving complex backend problems, integrating systems, and exploring new technologies. My experience spans Python (FastAPI, Django), JavaScript/TypeScript, DevOps (Ansible, Terraform, Docker, etc.) and UI frameworks like Vue.js and Svelte.<br>I bring a strong understanding of WebSockets, peer-to-peer connections, and cloud-native development using Docker and automation tools. Whether it’s designing APIs, optimizing data flow, or deploying services efficiently, I aim to deliver practical, high-quality solutions that make a real impact.";
